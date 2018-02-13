dc096269f835c4cbf2d4d46effef8f1d8483e460
hrd2910 <35139001+hrd2910@users.noreply.github.com>
2018-02-13 22:27:51 +0530

    Add files via upload

---
+++ generateList.py
@@ -0,0 +1,6 @@
+#Write a program which accepts a sequence of comma-separated numbers from console and generate a list.
+
+
+values = input("Enter a sequence of 5 comma-separated numbers : ")
+list = values.split(",")
+print('List : ',list)
